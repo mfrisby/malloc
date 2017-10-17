afd087204ace7a1597dd9bbec3a4d7644e8f46df
Morgane FRISBY <mfrisby@e1r8p4.42.fr>
2017-10-17 13:43:59 +0200

    UPD ZONE + FIX malloc

--- srcs/add_header.c
+++ srcs/add_header.c
@@ -1,14 +1,14 @@
 #include "../includes/malloc.h"
-t_header *add_header(t_header *h, int size, int can_free)
+t_header *add_header(t_header *h, int size, int can_free, int n)
 {
     h->can_free = can_free;
     h->size = size;
     h->is_free = 0;
     h->next = NULL;
-    if (!zones.head)
-        zones.head = h;
-    if (zones.tail)
-        zones.tail->next = h;
-    zones.tail = h;
+    if (!zones[n].head)
+        zones[n].head = h;
+    if (zones[n].tail)
+        zones[n].tail->next = h;
+    zones[n].tail = h;
     return (h);
 }
--- srcs/malloc.c
+++ srcs/malloc.c
@@ -1,62 +1,39 @@
 #include "../includes/malloc.h"
 
-static void *large_ptr(int size)
+static void *new_mem(int size, int n)
 {
     t_header *h;
     void *ptr;
-
+    
+    if (!zones[n].head)
+    {
+        zones[n].pages = 0;
+        zones[n].head = NULL;
+        zones[n].tail = NULL;
+    }
     ptr = ft_mmap(size + HEADERSIZE);
     h = ptr;
     if (!h)
-    {
-        ft_putendl("problem");
         return (NULL);
-    }
-    h = add_header(h, size + HEADERSIZE, 1);
+    h = add_header(h, size + HEADERSIZE, 1, 2);
     return ((void*)(h + 1));
 }
 
-// static void *get_free_space(size)
-// {
-//     // t_header *h;
-//     // int i;
+static void *get_free_space(int size, int n)
+{
+    t_header *h;
+    int i;
 
-//     // i = 0;
-//     // if (size <= (int)(TINYMAXSIZE))
-//     // {
-//     //     while (i < TINYZONE)
-//     //     {
-//     //         h = (t_header*)zones.ptiny + i;
-//     //         if (h)
-//     //         {
-//     //             if (h->is_free == 1)
-//     //             {
-//     //                 h->is_free = 0;
-//     //                 return (zones.ptiny + i);
-//     //             }
-//     //         }
-//     //         i += TINYMAXSIZE;
-//     //     }
-//     // }
-//     // else if (size <= (int)(SMALLMAXSIZE))
-//     // {
-//     //     while (i < TINYZONE)
-//     //     {
-//     //         h = (t_header*)zones.psmall + i;
-//     //         if (h)
-//     //         {
-//     //             if (h->is_free == 1)
-//     //             {
-//     //                 h->is_free = 0;
-//     //                 return (zones.psmall + i);
-//     //             }
-//     //         }
-//     //         i += SMALLMAXSIZE;
-//     //     }
-//     // }
-//     // ft_putendl("no place");
-//     return (large_ptr(size));
-// }
+    i = 0;
+    h = zones[n].head;
+    while (h)
+    {
+        if (h->is_free == 1)
+            return ((void*)(h + 1));
+        h = h->next;
+    }
+    return (new_mem(size, n));
+}
 
 static void *small_zone(int size)
 {
@@ -65,24 +42,28 @@ static void *small_zone(int size)
     
     i = 0;
     h = NULL;
-    if (!zones.psmall)
+    if (!zones[1].mem)
     {
-        zones.small_page = 0;
-        zones.psmall = ft_mmap(SMALLZONE);
+        zones[1].pages = 0;
+        zones[1].mem = ft_mmap(SMALLZONE);
+        zones[1].head = NULL;
+        zones[1].tail = NULL;
     }
-    while (i <= zones.small_page)
+    while (i <= zones[1].pages)
         i++;
-    i *= SMALLMAXSIZE;
+    i *= TOTALSMALLSIZE;
     if (i >= SMALLZONE)
-        return (NULL);//(large_ptr(size));
-    zones.small_page++;
-    h = zones.psmall + i;
-    if (!h)
     {
-        ft_putendl("problem");
-        return (NULL);
+        ft_putstr("\033[45ms\033[0m");
+        return (get_free_space(size, 1));
+        //return (new_mem(size, 1));
     }
-    h = add_header(h, size, 0);
+    ft_putstr("\033[33ms\033[0m");
+    h = zones[1].mem + zones[1].pages * TOTALTINYSIZE;
+    zones[1].pages++;
+    if (!h)
+        return (NULL);
+    h = add_header(h, size + HEADERSIZE, 0, 1);
     return (void*)(h + 1);
 }
 
@@ -93,24 +74,27 @@ static void *tiny_zone(int size)
 
     i = 0;
     h = NULL;
-    if (!zones.ptiny)
+    if (!zones[0].mem)
     {
-        zones.tiny_page = 0;
-        zones.ptiny = ft_mmap(TINYZONE);
+        zones[0].pages = 0;
+        zones[0].mem = ft_mmap(TINYZONE);
+        zones[0].head = NULL;
+        zones[0].tail = NULL;
     }
-    while (i <= zones.tiny_page)
+    while (i <= zones[0].pages)
         i++;
-    i *= TINYMAXSIZE;
+    i *= TOTALTINYSIZE;
     if (i >= TINYZONE)
-        return (NULL);//(large_ptr(size));
-    zones.tiny_page++;
-    h = zones.ptiny + zones.tiny_page * TOTALTINYZONE;
-    if (!h)
     {
-        ft_putendl("problem");
-        return (NULL);
+        ft_putstr("\033[45mt\033[0m");
+        return (new_mem(size, 0));
     }
-    h = add_header(h, size, 0);
+    ft_putstr("\033[30mt\033[0m");
+    h = zones[0].mem + zones[0].pages * TOTALTINYSIZE;
+    if (!h)
+        return (NULL);
+    h = add_header(h, size + HEADERSIZE, 0, 0);
+    zones[0].pages++;
     return (void*)(h + 1);
 }
 
@@ -121,19 +105,10 @@ void		*malloc(size_t size)
     if (size <= 0)
         return (NULL);
     else if (size <= TINYMAXSIZE)
-    {
         ptr = tiny_zone(size);
-        ft_putstr("e");
-    }
     else if (size <= SMALLMAXSIZE)
-    {
         ptr = small_zone(size);
-        ft_putstr("e");
-    }
     else
-    {
-        ft_putstr("l");
-        ptr = large_ptr(size);
-    }
+        ptr = new_mem(size, 2);
     return (ptr);
 }
