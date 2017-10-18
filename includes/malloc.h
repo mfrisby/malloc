00ceb3e3d615ecaae2118b038dccf4c1441ba51b
Morgane FRISBY <mfrisby@e1r9p9.42.fr>
2017-10-18 16:12:02 +0200

    norme

--- includes/malloc.h
+++ includes/malloc.h
@@ -6,7 +6,7 @@
 /*   By: mfrisby <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/17 10:58:59 by mfrisby           #+#    #+#             */
-/*   Updated: 2017/07/17 11:47:55 by mfrisby          ###   ########.fr       */
+/*   Updated: 2017/10/18 15:14:10 by mfrisby          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,48 @@
 # define MALLOC_H
 
 # include <unistd.h>
-# include <sys/mman.h>     
+# include <sys/mman.h>
 # include <stdlib.h>
-# include <sys/time.h> 
+# include <sys/time.h>
 # include <sys/resource.h>
 # include <sys/mman.h>
 
-#define HEADERSIZE      sizeof(t_header)
+# define PAGESIZE	getpagesize()
+# define NBALLOC	200
+# define HEADERSIZE sizeof(t_header)
+# define TINYSIZE 	1024
+# define TINYZONE	PAGESIZE * NBALLOC
+# define SMALLSIZE	2048
+# define SMALLZONE	PAGESIZE * NBALLOC
 
-#define NBALLOC         150
+typedef struct			s_header {
+	unsigned			is_free;
+	unsigned			can_free;
+	int					size;
+	struct s_header		*next;
+}						t_header;
 
-typedef struct          s_header {
-	unsigned            is_free;
-	unsigned            can_free;
-    int                 size;
-    struct s_header     *next;
-}                       t_header;
-
-typedef struct             s_zones
+typedef struct			s_zones
 {
-    int                    pages;
-    void                   *mem;
-    t_header               *head;
-    t_header               *tail;
-}                          t_zones;
+	int					pages;
+	void				*mem;
+	t_header			*head;
+	t_header			*tail;
+}						t_zones;
 
-t_zones                     zones[3];
+t_zones					zones[3];
 
-void		free(void *ptr);
-void		*malloc(size_t size);
-void		*realloc(void *ptr, size_t size);
-void        *ft_mmap(int size);
-void        show_alloc_mem();
-void	    ft_putendl(char const *s);
-void	    ft_putnbr(int n);
-void	    ft_putstr(char const *s);
-void	    ft_putchar(char c);
-void        aff_mem_addr(int nb, int base);
-void        *ft_memcpy(void *dst, const void *src, size_t n);
-t_header    *add_header(t_header *h, int size, int can_free, int n);
-//char		*ft_utoa_base(unsigned long long nb, int base);
-
+void					free(void *ptr);
+void					*malloc(size_t size);
+void					*realloc(void *ptr, size_t size);
+void					*ft_mmap(int size);
+void					show_alloc_mem();
+void					ft_putendl(char const *s);
+void					ft_putnbr(int n);
+void					ft_putstr(char const *s);
+void					ft_putchar(char c);
+void					aff_mem_addr(size_t dec);
+void					*ft_memcpy(void *dst, const void *src, size_t n);
+t_header				*add_header(t_header *h, int size, int can_free, int n);
 
 #endif
